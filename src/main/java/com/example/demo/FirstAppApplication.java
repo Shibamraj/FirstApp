cb5374535b1b8c447b8facbcba93f2bae1281bd7
Shibam Raj Mohanty <Shibam Raj Mohanty@Shibam>
2022-06-26 11:39:42 +0530

    This is forth time

--- src/main/java/com/example/demo/FirstAppApplication.java
+++ src/main/java/com/example/demo/FirstAppApplication.java
@@ -5,9 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FirstAppApplication {
+	private static int a=10;
 
 	public static void main(String[] args) {
 		SpringApplication.run(FirstAppApplication.class, args);
+		
+		System.out.println(a);
 		System.out.println("I am here");
 		System.out.println("look");
 	}
